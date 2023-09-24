6203a31f3e7a9fd5d1f0c3f91be4e3d8dd3d3b9c
CasN <cas@nuy.info>
2023-09-24 11:36:26 +0200

    Update all_tasks_xls.php

--- pages/all_tasks_xls.php
+++ pages/all_tasks_xls.php
@@ -40,7 +40,7 @@ xmlns="http://www.w3.org/TR/REC-html40">
 
 <?php
 # Pull all Tasks-Record entries for the current user
-$query = "SELECT a.*,b.taskcat_title FROM {plugin_Tasks_defined}le as a,{plugin_Tasks_cat}e as b  WHERE a.taskcat_id=b.taskcat_id and task_completed = '0000-00-00 00:00:00' ORDER BY task_handler,task_due ASC";
+$query = "SELECT a.*,b.taskcat_title FROM {plugin_Tasks_defined} as a,{plugin_Tasks_cat}e as b  WHERE a.taskcat_id=b.taskcat_id and task_completed = '0000-00-00 00:00:00' ORDER BY task_handler,task_due ASC";
 $result = db_query($query);
 while ($row = db_fetch_array($result)) {
 	?>
