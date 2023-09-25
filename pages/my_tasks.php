5286e05f94390dfa41ace231c93c310e7dc3bcf4
CasN <cas@nuy.info>
2023-09-25 21:02:53 +0200

    Update my_tasks.php

--- pages/my_tasks.php
+++ pages/my_tasks.php
@@ -7,9 +7,7 @@
 #
 ########################################################
 $user 			= auth_get_current_user_id();
-# what is the table for tasks
 require_once( config_get( 'plugin_path' ) . 'Tasks' . DIRECTORY_SEPARATOR . 'Tasks_api.php' );  
-
 layout_page_header(  );
 layout_page_begin(  );
 $colspan=6;
@@ -33,8 +31,6 @@ $colspan=6;
 <td class="center" colspan="6">
 <br>
 <td colspan="<?php echo $colspan ?>" class="row-category"><div align="left"><a name="taskrecord"></a>
-
-
 </div>
 </td>
 </tr>
@@ -74,32 +70,31 @@ while ($row = db_fetch_array($result)) {
 }
 $use_groups		= config_get( 'plugin_Tasks_tasks_assign_group' );
 if (( ON == $use_groups) and (check_user_in_group($user))){
-$ugrp_table	= plugin_table('usergroup','Usergroups');
-$query2 = "SELECT * FROM {plugin_Tasks_defined},{plugin_Usergroups_ugroup}  WHERE task_group= group_id and user_id = $user  and task_completed = '0000-00-00 00:00:00' ORDER BY task_due ASC";
-$result2 = db_query($query2);
-while ($row = db_fetch_array($result2)) {
-	?>
-	<tr >
-	<td><div align="center"><?php  echo html_entity_decode($row["task_title"]); ?>
-	<br>
-	<?php  echo date("d.m.Y", strtotime($row["task_due"])); ?></div></td>
-	<td><?php echo group_get_name($row["task_group"]); ?></td>
-	<td><div align="left">
-	<?PHP
-	echo html_entity_decode($row["task_desc"]);
-	?>
- 	</div></td>
-	<td><div align="center">
-	<?PHP
-	echo html_entity_decode($row["task_response"]);
-	?>
-	</div></td>
-	<td><div align="center">
-	<a href="view.php?id=<?php echo $row['bug_id'];?>"><?php echo lang_get( 'issue_task' ) ?></a><br>
-	</div></td>
-	</tr>
-	<?php
-}
+	$query2 = "SELECT * FROM {plugin_Tasks_defined},{plugin_Usergroups_ugroup}  WHERE task_group= group_id and user_id = $user  and task_completed = '0000-00-00 00:00:00' ORDER BY task_due ASC";
+	$result2 = db_query($query2);
+	while ($row = db_fetch_array($result2)) {
+		?>
+		<tr >
+		<td><div align="center"><?php  echo html_entity_decode($row["task_title"]); ?>
+		<br>
+		<?php  echo date("d.m.Y", strtotime($row["task_due"])); ?></div></td>
+		<td><?php echo group_get_name($row["task_group"]); ?></td>
+		<td><div align="left">
+		<?PHP
+		echo html_entity_decode($row["task_desc"]);
+		?>
+ 		</div></td>
+		<td><div align="center">
+		<?PHP
+		echo html_entity_decode($row["task_response"]);
+		?>
+		</div></td>
+		<td><div align="center">
+		<a href="view.php?id=<?php echo $row['bug_id'];?>"><?php echo lang_get( 'issue_task' ) ?></a><br>
+		</div></td>
+		</tr>
+		<?php
+	}	
 }	 
 ?>
 </table></div></div></div></div>
